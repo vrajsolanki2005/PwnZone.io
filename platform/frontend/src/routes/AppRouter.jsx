1d70c8d4dd5699ff3781da364508c11ff2f13b7b
vrajsolanki2005 <144586038+vrajsolanki2005@users.noreply.github.com>
2026-06-12 00:55:11 +0530

    Update AppRouter.jsx

--- platform/frontend/src/routes/AppRouter.jsx
+++ platform/frontend/src/routes/AppRouter.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import DashboardLayout from '../layouts/DashboardLayout'
 import Dashboard from '../pages/Dashboard/Dashboard'
 import Labs from '../pages/Labs'
+// import Leaderboard from '../pages/Leaderboard'
+// import Profile from '../pages/Profile'
+// import Settings from '../pages/Settings'
 
 export default function AppRouter() {
   return (
@@ -10,6 +13,9 @@ export default function AppRouter() {
         <Route element={<DashboardLayout />}>
           <Route path="/" element={<Dashboard />} />
           <Route path="/labs" element={<Labs />} />
+          {/* <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/settings" element={<Settings />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
